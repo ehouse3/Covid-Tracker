e58e96f5e59d2ac3b9865d3abf334530deea9bb5
ehouse3 <houseesh@gmail.com>
2025-06-11 13:42:10 -0400

    ascend descend functionality

--- covid-dashboard/app/page.tsx
+++ covid-dashboard/app/page.tsx
@@ -15,12 +15,14 @@ export default function Dashboard() {
   function renderState(state:state, i:number) {
     return (
       <div key={state.id} className="flex flex-row justify-between flex-wrap border-4 border-sky-700 bg-sky-800 rounded-xl mx-5 my-5">
-        <h2 className="basis-3/4">{state.abbrev}</h2>
-        <button className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500" >up</button>
-        <button className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500" >down</button>
+        <h2 className="basis-3/4">{state.abbrev} {state.id}</h2>
+        <button className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500"
+          onClick={(event) => {ascendState(state)}} >up</button>
+        <button className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500" 
+          onClick={(event) => {descendState(state)}} >down</button>
         <button className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500" 
           onClick={(event) => {removeState(state.id)}}>Remove</button>
-        <div className="basis-full">
+        <div className="basis-full p-2">
           Graph
         </div>
       </div>
@@ -41,8 +43,26 @@ export default function Dashboard() {
     setStates(states.filter(s => s.id !== id));
   }
 
-  function swapState(a:state, b:state) {
-    ;
+  function ascendState(s:state) {
+    let index = states.findIndex(st => st.id === s.id);
+    if (index !== 0) {
+      const newStates = [...states];
+      const temp:state = newStates[index];
+      newStates[index] = newStates[index - 1];
+      newStates[index - 1] = temp;
+      setStates(newStates);
+    }
+  }
+
+  function descendState(s:state) {
+    let index = states.findIndex(st => st.id === s.id);
+    if (index !== states.length) {
+      const newStates = [...states];
+      const temp:state = newStates[index];
+      newStates[index] = newStates[index + 1];
+      newStates[index + 1] = temp;
+      setStates(newStates);
+    }
   }
 
   return (
@@ -53,7 +73,7 @@ export default function Dashboard() {
         <div>
           <button
             className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500"
-            onClick={(event) => {addState({id:null, abbrev:"CA"});}}
+            onClick={(event) => {addState({id:null, abbrev:"ST"});}}
           >
             Add state
           </button>
