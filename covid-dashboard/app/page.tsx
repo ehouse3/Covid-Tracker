3c7b5d538ca3cf189473e70717088c4d19dcc867
ehouse3 <54383796+ehouse3@users.noreply.github.com>
2025-06-20 14:59:48 -0400

    Metric Dropdown Selection
    still needs functionality

--- covid-dashboard/app/page.tsx
+++ covid-dashboard/app/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 import { useState, useRef } from 'react';
 import { LineChart } from "@mui/x-charts";
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+
 import { datum ,fetchState } from "../parser.js";
 // import { DateTime } from 'luxon'; 
 
 export default function Dashboard() {
   const nextId = useRef(0); 
   interface state {
-    id: number | undefined,
-    abbrev: string,
-    data?: datum[]
+    id: number | undefined, //unique
+    abbrev: string, 
+    data?: datum[], // array of state data from csv
+    selectedMetrics?: string[] // selected data to be displayed by the graph
   }
 
   const [states, setStates] = useState<state[]>([]);
@@ -32,12 +38,14 @@ export default function Dashboard() {
       <div key={state.id} className="flex flex-row justify-between flex-wrap border-4 border-sky-700 bg-sky-800 rounded-xl mx-5 my-5">
 
         <div className="flex flex-row w-full">
-          <div className="w-1/3"></div>
+          <div className="w-1/3">
+            <MetricDropdown state={state}/>
+          </div>
           <h2 className="w-1/3 flex flex-row flex-nowrap justify-center text-4xl">{state.abbrev}</h2>
           <div className="w-1/3 flex flex-row flex-nowrap justify-end">
-            <StateButton callBack={ascendState} s={state} innerHTML='Up'/>
-            <StateButton callBack={descendState} s={state} innerHTML='Down'/>
-            <StateButton callBack={removeState} s={state} innerHTML='Remove'/>
+            <StateButton callBack={ascendState} state={state} innerHTML='Up'/>
+            <StateButton callBack={descendState} state={state} innerHTML='Down'/>
+            <StateButton callBack={removeState} state={state} innerHTML='Remove'/>
           </div>
         </div>
 
@@ -68,23 +76,57 @@ export default function Dashboard() {
 
   /** StateButton Component that displays a button with callback function for press. */
   function StateButton(
-    { callBack, s ,innerHTML}: 
-    { callBack:(state: state) => void, s:state, innerHTML: string }) {
+    { callBack, state ,innerHTML}: 
+    { callBack:(s:state) => void, state:state, innerHTML: string }) {
     return (
       <button className="px-3 m-1 border-2 rounded-md border-sky-500 bg-sky-700 hover:bg-blue-600 hover:border-blue-500"
-        onClick={() => { callBack(s) }} >{innerHTML}</button>
+        onClick={() => { callBack(state) }} >{innerHTML}</button>
     )
   }
 
-  //https://react.dev/reference/react/useState 
-  /** Adds state to active state list w/ unique id */
+  const dataList:string[] = ["death","deathConfirmed","deathIncrease","deathProbable","hospitalized","hospitalizedCumulative","hospitalizedCurrently","hospitalizedIncrease","inIcuCumulative","inIcuCurrently","negative","negativeIncrease","negativeTestsAntibody","negativeTestsPeopleAntibody","negativeTestsViral","onVentilatorCumulative","onVentilatorCurrently","positive","positiveCasesViral","positiveIncrease","positiveScore","positiveTestsAntibody","positiveTestsAntigen","positiveTestsPeopleAntibody","positiveTestsPeopleAntigen","positiveTestsViral","recovered","totalTestEncountersViral","totalTestEncountersViralIncrease","totalTestResults","totalTestResultsIncrease","totalTestsAntibody","totalTestsAntigen","totalTestsPeopleAntibody","totalTestsPeopleAntigen","totalTestsPeopleViral","totalTestsPeopleViralIncrease","totalTestsViral","totalTestsViralIncrease"];
+  const dataListPretty:string[] = ["Death","deathConfirmed","deathIncrease","deathProbable","hospitalized","hospitalizedCumulative","hospitalizedCurrently","hospitalizedIncrease","inIcuCumulative","inIcuCurrently","negative","negativeIncrease","negativeTestsAntibody","negativeTestsPeopleAntibody","negativeTestsViral","onVentilatorCumulative","onVentilatorCurrently","positive","positiveCasesViral","positiveIncrease","positiveScore","positiveTestsAntibody","positiveTestsAntigen","positiveTestsPeopleAntibody","positiveTestsPeopleAntigen","positiveTestsViral","recovered","totalTestEncountersViral","totalTestEncountersViralIncrease","totalTestResults","totalTestResultsIncrease","totalTestsAntibody","totalTestsAntigen","totalTestsPeopleAntibody","totalTestsPeopleAntigen","totalTestsPeopleViral","totalTestsPeopleViralIncrease","totalTestsViral","totalTestsViralIncrease"];
+  /** Dropdown component to select which metrics of data to display */
+  function MetricDropdown(
+    { state }:
+    { state:state}
+  ) 
+  {
+    return (
+      <div className="m-2 border-2 rounded-md border-sky-500 bg-sky-700">
+        <FormControl fullWidth>
+          <InputLabel className="bg-sky-700" id="demo-simple-select-label"><div className="px-2 text-white">Select Metrics</div></InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            multiple
+            value={state.selectedMetrics}
+            onChange={(e) => handleMetricDropdownChange(e, state)}
+          >
+            {dataList.map((val, index) => <MenuItem value={val} key={val}>{dataListPretty[index]}</MenuItem>)}
+          </Select>
+        </FormControl>
+      </div>
+    )
+  }
+
+  /** Handles onChange for MetricDropDownComponent. Updates corresponding state's selectedMetrics */
+  function handleMetricDropdownChange(e: SelectChangeEvent<string[]>, s: state) {
+    const metrics = e.target.value as string[];
+    const index = states.findIndex(st => st.id === s.id);
+    const newStates = [...states];
+    newStates[index].selectedMetrics = metrics;
+    setStates(newStates);
+  }
+
+  /** Adds state to active state list w/ unique id. Fetches state data as well */
   async function addState(newState:state) {
     if(states.length >= 5) { return; }
 
     const stateData:datum[] = await fetchState(newState.abbrev)
     console.log("page.tsx fetch state return",stateData); 
 
-    setStates([...states, { ...newState, id: nextId.current++, data:stateData}]); // Being called multiple times. useEffect might fix?
+    setStates([...states, { ...newState, id: nextId.current++, data:stateData, selectedMetrics:[]}]); // Being called multiple times. useEffect might fix?
   }
 
   /** Removes active state from state list */
@@ -116,13 +158,7 @@ export default function Dashboard() {
     }
   }
 
-  const acceptStates = [
-  "AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE",
-  "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY",
-  "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MP", "MS", "MT",
-  "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK",
-  "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA",
-  "VI", "VT", "WA", "WI", "WV", "WY" ];
+  const acceptStates = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY" ];
   /** Handles submit event, calling addState if appropriate */
   function handleSubmit(e:React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
